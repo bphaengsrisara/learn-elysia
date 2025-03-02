093d89fdd5fbe6857828db286e5c6d78993da766
Bovonrajt Phaengsrisara <bovonrajt.p@gmail.com>
2025-03-02 17:36:42 +0700

    feat(doc): add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
 import { config } from "./config";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(config.port);
+const app = new Elysia()
+  .use(swagger())
+  .get("/", () => "Hello Elysia")
+  .listen(config.port);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
