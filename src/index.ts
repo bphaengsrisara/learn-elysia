30af14badd79658247675c94e45d574a8d289bb8
Bovonrajt Phaengsrisara <bovonrajt.p@gmail.com>
2025-03-02 16:25:24 +0700

    feat(config): try to use .env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Elysia } from "elysia";
+import { config } from "./config";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia").listen(config.port);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
